3338c4ca2a32696e0a64ea8851fbe2092f0c59ea
Abmad <abdelmoghit-madih@hotmail.com>
2017-11-30 16:07:46 +0100

    update tablexico

--- Table_lexico/tablexico.c
+++ Table_lexico/tablexico.c
@@ -1,16 +1,16 @@
 #include "tablexico.h"
-#include "../Arbre/arbre.h"
-lexeme table_lexico[LONGUEUR];
+
+lexeme table_lexico[longueur];
 
 int longueur_int(int valeur){ //Calcule la longueur d'un entier
     int retour = 0;
-    
+
     if(valeur < 0)
     {
         retour++;
         valeur = -1 * valeur;
     }
-    
+
     while(valeur !=0)
     {
         retour++;
@@ -22,40 +22,111 @@ int longueur_int(int valeur){ //Calcule la longueur d'un entier
 
 //ajoute un lexème dans la table lexicographique
 int add_lexeme_char(char* lex){//Créé par Dan Robert Tsoumbou Moutimba
-return 200;
-    assert(cpt<LONGUEUR);
-    if(table_lexico[cpt].longueur==0 && table_lexico[cpt].exp_lexeme_char!=lex){
-        lexeme l;
-        strcpy(l.exp_lexeme_char,lex);
-        l.longueur = strlen(lex);
-        l.suiv = &(table_lexico[cpt+1]);
-        l.hash_code=cpt+1;
-        table_lexico[cpt] = l;
-        
-        return l.hash_code;
-    }
-    else if(table_lexico[cpt].longueur==strlen(lex) && table_lexico[cpt].exp_lexeme_char==lex){
-        //lexème déjà présent dans la table, aucune action n'est effectuée.
-	return 9999;
-    }
-    else{
-        cpt = cpt +1;
-        add_lexeme_char(lex);
-    }
-    cpt=0;
-    
+    assert(cpt<longueur);
+        if(table_lexico[cpt].longueur==0 && table_lexico[cpt].exp_lexeme_char!=lex){
+            lexeme l;
+            l.exp_lexeme_char = lex;
+            l.exp_lexeme_int = 0;
+            l.exp_lexeme_double = 0;
+            l.longueur = strlen(lex);
+            l.suiv = &table_lexico[cpt+1];
+            l.hash_code=cpt+1;
+            table_lexico[cpt] = l;
+
+            return l.hash_code;
+        }
+        else if(table_lexico[cpt].longueur==strlen(lex) && table_lexico[cpt].exp_lexeme_char==lex){
+            return table_lexico[cpt1].hash_code;
+        }
+        else{
+            cpt = cpt +1;
+            add_lexeme_char(lex);
+        }
+        cpt=0;
 }
 
+int add_lexeme_int(int lex){//Créé par Dan Robert Tsoumbou Moutimba
+    assert(cpt2<longueur);
+        if(table_lexico[cpt2].longueur==0 && table_lexico[cpt2].exp_lexeme_int!=lex){
+            lexeme l;
+            l.exp_lexeme_int = lex;
+            l.exp_lexeme_char = NULL;
+            l.exp_lexeme_double = 0;
+            l.longueur = longueur_int(lex);
+            l.suiv = &table_lexico[cpt2+1];
+            l.hash_code = cpt2 +1;
+            table_lexico[cpt2] = l;
+
+            return l.hash_code;
+        }
+        else if(table_lexico[cpt2].longueur==longueur_int(lex) && table_lexico[cpt2].exp_lexeme_int==lex){
+            return table_lexico[cpt2].hash_code;
+        }
+        else{
+            cpt2 = cpt2 +1;
+            add_lexeme_int(lex);
+        }
+        cpt2=0;
+}
+
+int add_lexeme_double(double lex){//Créé par Dan Robert Tsoumbou Moutimba
+    assert(cpt3<longueur);
+        if(table_lexico[cpt3].longueur==0 && table_lexico[cpt3].exp_lexeme_int!=lex){
+            lexeme l;
+            l.exp_lexeme_double = lex;
+            l.exp_lexeme_char = 0;
+            l.exp_lexeme_int=0;
+            l.longueur = longueur_int((int) lex);
+            l.suiv = &table_lexico[cpt3+1];
+            l.hash_code = cpt3 +1;
+            table_lexico[cpt3] = l;
+
+            return l.hash_code;
+        }
+        else if(table_lexico[cpt3].longueur==longueur_int(lex) && table_lexico[cpt3].exp_lexeme_double==lex){
+            return table_lexico[cpt3].hash_code;
+        }
+        else{
+            cpt3 = cpt3 +1;
+            add_lexeme_double(lex);
+        }
+        cpt3=0;
+}
+
+void init_tab_lexico(){
+    add_lexeme_char("int");
+    add_lexeme_char("float");
+    add_lexeme_char("bool");
+    add_lexeme_char("char");
+    int i=4;
+    for(i=4;i<longueur;i++){
+        table_lexico[i].exp_lexeme_char = malloc(sizeof(char));
+        if(table_lexico[i].exp_lexeme_char==NULL){
+            exit(-1);
+        }
+        table_lexico[i].suiv = malloc(sizeof(lexeme));
+        if(table_lexico[i].suiv==NULL){
+            exit(-1);
+        }
+        table_lexico[i].exp_lexeme_double = 0.0;
+        table_lexico[i].exp_lexeme_int = 0;
+        table_lexico[i].longueur = 0;
+        table_lexico[i].hash_code = -1;
+    }
+}
 
 //affiche le contenu de la table jusqu'à la j-ieme case
-void affiche_table_lexico(int j){//Créé par Dan Robert Tsoumbou Moutimba
-    assert(j<LONGUEUR);
+void affiche_table_lexico(){//Créé par Dan Robert Tsoumbou Moutimba
+
     int i=0;
-    for(i=0;i<j;i++){
-        printf("CASE: %d, %s, \n",table_lexico[i].hash_code, table_lexico[i].exp_lexeme_char);
+    for(i=0;i<longueur;i++){
+            if(table_lexico[i].hash_code==-1) {return;}
+            else {
+                printf("%d, %s, %d, %f\n",table_lexico[i].hash_code,
+                    table_lexico[i].exp_lexeme_char,
+                    table_lexico[i].exp_lexeme_int,
+                    table_lexico[i].exp_lexeme_double);
+            }
     }
 }
-void init_tab_lex(){
-    
-    printf("init\n");
-}
+
--- Table_lexico/tablexico.h
+++ Table_lexico/tablexico.h
@@ -6,23 +6,28 @@
 
 typedef struct {
     char* exp_lexeme_char;//expression du lexème
+    int exp_lexeme_int;
+    double exp_lexeme_double;
     int longueur;//nombre de caractères du lexème
     struct lexeme * suiv;//lexème suivant
     int hash_code;
 } lexeme;
 
-#define LONGUEUR 250
+#define longueur 250;
 
 int cpt, cpt2, cpt3;
 
-lexeme table_lexico[LONGUEUR];
+lexeme table_lexico[longueur];
 
 int longueur_int(int valeur);
 
 int add_lexeme_char(char* lex);
 
+int add_lexeme_int(int lex);
+
+int add_lexeme_double(double lex);
+
 void affiche_table_lexico(int j);
 
-void init_tab_lex();
-
+void init_tab_lexico();
 
