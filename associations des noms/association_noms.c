24195b0163c81a389058cbdf921cc15d17046318
Abmad <abdelmoghit-madih@hotmail.com>
2017-11-30 11:16:19 +0100

    init association des noms

---
+++ associations des noms/association_noms.c
@@ -0,0 +1,9 @@
+//
+//  association_noms.c
+//  Compilation
+//
+//  Created by Abdelmoghit MADIH on 11/30/17.
+//  Copyright © 2017 Abdelmoghit MADIH. All rights reserved.
+//
+
+#include "association_noms.h"
---
+++ associations des noms/association_noms.h
@@ -0,0 +1,14 @@
+//
+//  association_noms.h
+//  Compilation
+//
+//  Created by Abdelmoghit MADIH on 11/30/17.
+//  Copyright © 2017 Abdelmoghit MADIH. All rights reserved.
+//
+
+#ifndef association_noms_h
+#define association_noms_h
+
+#include <stdio.h>
+
+#endif /* association_noms_h */
